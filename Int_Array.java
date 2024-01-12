97a50688b1727553078d46f6753198f1d81b9fe0
Akash-Shilimkar <99742784+Akash-Shilimkar@users.noreply.github.com>
2024-01-12 20:07:48 +0530

    Create an array of Int type

--- Int_Array.java
+++ Int_Array.java
@@ -3,15 +3,17 @@
 public class Int_Array{
     public static void main(String[] args)
     {
-        int a [] = new int[5];
+        int a [] = new int[10];
         a[0]=10;
         a[1]=70;
         a[2]=80;
         a[3]=50;
         a[4]=40;
         a[5]=30;
-
-        System.out.println(a[1]);
-
+        a[6]=20;
+        a[7]=60;
+        a[8]=100;
+        a[9]=200;
+        
     }
 }
