895bbdf00c82ef17210bedc46405b43a2d0ec9f6
Akash-Shilimkar <99742784+Akash-Shilimkar@users.noreply.github.com>
2024-01-12 19:58:22 +0530

    Create an array of char type

---
+++ Char_Array.java
@@ -0,0 +1,21 @@
+//Create an array of char type an store 10 elements in array
+
+public class Char_Array{
+    public static void main(String[] args)
+    {
+       
+        char a [] = new char[10];
+        a[0]='A';
+        a[1]='B';
+        a[2]='C';
+        a[3]='D';
+        a[4]='E';
+        a[5]='F';
+        a[6]='G';
+        a[7]='H';
+        a[8]='I';
+        a[9]='J';
+
+        System.out.println(a[9]);
+    }
+}
