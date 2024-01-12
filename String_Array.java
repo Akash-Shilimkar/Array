9bd77553b4e50af819ead00f137187c6d509f970
Akash-Shilimkar <99742784+Akash-Shilimkar@users.noreply.github.com>
2024-01-12 20:05:48 +0530

    Cerate array of an String type

---
+++ String_Array.java
@@ -0,0 +1,8 @@
+public class String_Array{
+    public static void main(String[] args)
+    {
+       
+        String name [] = {"Akash","Mayur","Ganesh","Sarthak","Varsha","Trupti","Amar","Aniket","Rutuja","Dhanashri"};
+        
+    }
+}
